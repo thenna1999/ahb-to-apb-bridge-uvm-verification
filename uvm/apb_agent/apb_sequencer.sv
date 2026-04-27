5c59ce8183844aa4af1073b10d9c20c4daa56562
Thennarasu_Chandrasekaran <87271927+thenna1999@users.noreply.github.com>
2026-04-27 12:02:10 +0530

    Add apb_sequencer class for APB transactions

---
+++ uvm/apb_agent/apb_sequencer.sv
@@ -0,0 +1,14 @@
+class apb_sequencer extends uvm_sequencer#(apb_txn);
+
+	`uvm_component_utils(apb_sequencer)
+
+	function new(string name="apb_sequencer",uvm_component parent);
+		super.new(name,parent);
+	endfunction
+
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+	endfunction
+
+endclass
+
