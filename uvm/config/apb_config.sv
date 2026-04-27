d772110c9e1277753d31dcb61eae99f464ebd946
Thennarasu_Chandrasekaran <87271927+thenna1999@users.noreply.github.com>
2026-04-27 11:42:41 +0530

    Add apb_config class for APB configuration

---
+++ uvm/config/apb_config.sv
@@ -0,0 +1,13 @@
+class apb_config extends uvm_object;
+
+	`uvm_object_utils(apb_config)
+
+	function new(string name="apb_config");
+		super.new(name);
+	endfunction
+
+	uvm_active_passive_enum is_active;
+	virtual apb_if vif;
+
+endclass
+
